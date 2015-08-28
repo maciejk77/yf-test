570ab05579f5e9d6c8098a63f5b02901e1ccd12a
Maciej Kurek <maciejk77@gmail.com>
2015-08-28 11:54:08 +0200

    Circle creation completed, fixed canvas coordinatres for the moment

--- js/app.js
+++ js/app.js
@@ -6,6 +6,23 @@ function storePosition(event) {
   xyCoord.push(x);
   xyCoord.push(y);
   console.log(xyCoord);
+  createCircle();
+}
+
+function createCircle() {
+      var canvas = document.getElementById('myCanvas');
+      var context = canvas.getContext('2d');
+      var centerX = canvas.width / 2;
+      var centerY = canvas.height / 2;
+      var radius = 40;
+
+      context.beginPath();
+      context.arc(x, y, radius, 0, 2 * Math.PI, false);
+      context.fillStyle = 'gray';
+      context.fill();
+      context.lineWidth = 1;
+      context.strokeStyle = 'black';
+      context.stroke();
 }
 
 var el = document.getElementById('body');      
