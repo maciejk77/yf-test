305ea7a0f8232e8936df2f57b3a7bfa35b69d2cd
Maciej Kurek <maciejk77@gmail.com>
2015-08-30 12:18:12 +0200

    Final refactoring

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 $(function() {
 
   var xyCoord = [];
+  var canvas = document.getElementById('myCanvas');
 
   function createCircle() {
-    var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
     var radius = 40;
+
     context.beginPath();
     context.arc(x, y, radius, 0, 2 * Math.PI, false);
     context.fillStyle = 'coral';
-    context.fill();
-    context.lineWidth = 1;
     context.strokeStyle = 'red';
+    context.lineWidth = 1;
+    context.fill();
     context.stroke();
   }
 
   function pushToCircle(returnedLetter) {
-    var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext("2d");
+    
     ctx.font = "30px Verdana";
     ctx.fillStyle = "black"; 
     ctx.fillText(returnedLetter, xyCoord[0] - 10 , xyCoord[1] + 10);
@@ -35,12 +34,12 @@ $(function() {
      
   $('#letters').on('click', function() {
     var formLetters = document.getElementById('letters').value;
+    var letterArray = formLetters.split('');
+    
     if(formLetters !== '') {
-      var letterArray = formLetters.split('');
       returnedLetter = letterArray.shift();
-      var text = document.getElementById('letters').value;  
-      text = text.substr(1);
-      document.getElementById('letters').value = text;
+      formLetters = formLetters.substr(1);
+      document.getElementById('letters').value = formLetters;
       pushToCircle(returnedLetter);
     };
   });    
